60799eb6acd315443d7712fd7bdf551c820a928d
kurado2039 <kuraudo.2039@gmail.com>
2023-07-01 16:05:50 +0900

    動作確認

--- index.js
+++ index.js
@@ -20,4 +20,37 @@ window.addEventListener('DOMContentLoaded', function(){
       document.getElementById("ss").href = imgData;
     }
   });
+
+  document.getElementById("generateBingo").addEventListener("click", (event) => {
+    let numbers = new Array(75).fill().map((item, index) => index+1);
+    console.log(numbers);
+    let tables = new Array(5);
+    for(let i = 0; i < 5; i++){
+      tables[i] = new Array(5);
+      for(let j = 0; j < 5; j++){
+        if(i == 2 && j == 2){/* 真ん中を除外 */
+        tables[i][j] = '';
+      }
+        else{
+          tables[i][j] = numbers.splice(Math.floor(Math.random() * numbers.length), 1)[0];
+        }
+      }
+    }
+
+    let bingoHtml='';
+    let rowHtml='';
+    let CellHtml='';
+    for(let i = 0; i < tables.length; i++){
+      for(let j = 0; j < tables[i].length; j++){
+        rowHtml+=`<td class="bingo_cell">${tables[i][j]}</td>`;
+      }
+      bingoHtml+=`<tr class="bingo_row">${rowHtml}</tr>`
+      rowHtml = '';
+      console.log(bingoHtml);
+    }
+
+    document.getElementById("targetBingo").innerHTML=bingoHtml;
+
+    console.log(tables);
+  })
 });
